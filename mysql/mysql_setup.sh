9fc00cfc35521521e550b05545f34f4f4d34f544
Alif Rachmawadi <subosito@gmail.com>
2013-11-19 15:00:29 +0700

    fix mysql setup

--- mysql/mysql_setup.sh
+++ mysql/mysql_setup.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 MYSQL_PASSWORD=password
 
 apt-get install -y expect
@@ -6,15 +6,13 @@ apt-get install -y expect
 mysqld_safe &
 sleep 5s
 
-mysqladmin -u root password $MYSQL_PASSWORD
-
 MYSQL_SECURE=$(expect -c "
 
 set timeout 10
 spawn mysql_secure_installation
 
 expect \"Enter current password for root (enter for none):\"
-send \"$MYSQL_PASSWORD\r\"
+send \"\r\"
 
 expect \"Change the root password?\"
 send \"n\r\"
@@ -35,6 +33,8 @@ expect eof
 ")
 
 echo "$MYSQL_SECURE"
+
+echo "GRANT ALL ON *.* TO admin@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql
 
 apt-get remove -y expect
 apt-get autoremove -y
